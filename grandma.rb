f89c6343339a3d869beb867de51bcda6e203fa2b
Emily Wagner <emilywagner598@gmail.com>
2019-01-16 18:45:06 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,9 +15,9 @@
 def speak_to_grandma (phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TO PUMPKIN!"
-  elseif !phrase == phrase.upcase
-    "HUH?! SPEAK UP, SONNY!"
-  else
+  elseif phrase == phrase.upcase
     "NO, NOT SINCE 1938!"
+  else
+    "HUH?! SPEAK UP, SONNY!"
   end
 end
